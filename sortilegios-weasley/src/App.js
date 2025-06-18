afa6b84e47810cc6931f9df83db1cc5938a733a7
SebastianSotoAr <ssotoa@unal.edu.co>
2025-06-18 00:06:04 -0500

    NavBar created

--- sortilegios-weasley/src/App.js
+++ sortilegios-weasley/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 import ProductsPage from "./pages/ProductsPage";
 import "./styles/magicCursor.css";
 
@@ -11,6 +12,7 @@ function App() {
         <h1 className="text-3xl font-bold my-4">Sortilegios Weasley</h1>
       </header>
       <main className="p-6">
+        <NavBar />
         <ProductsPage />
       </main>
     </div>
---
+++ sortilegios-weasley/src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+const NavBar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="/logo192.png" alt="Logo de Sortilegios Weasley" width="30" height="24"/>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse bg-body-secondary rounded-2 px-5" id="navbarNav">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="px-3">
+                    <button type="button" class="btn btn-primary mx-1">Iniciar Sesión</button>
+                    <button type="button" class="btn btn-primary mx-1">Carrito</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
